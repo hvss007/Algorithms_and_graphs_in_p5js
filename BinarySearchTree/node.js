f45254f75fe95a02fd2b38f92e3452c41f699380
Harsh Vardhan <hvss007@gmail.com>
2020-02-17 10:30:58 +0530

    Binary Search Basic

---
+++ BinarySearchTree/node.js
@@ -0,0 +1,35 @@
+Node.prototype.addNode=function(n){
+    if(n.value<this.value){
+        if(this.left===null){
+            this.left=n
+        }
+        else{
+            this.left.addNode(n);
+        }
+        
+    }
+    else if(n.value>this.value){
+        if(this.right===null){
+            this.right=n
+        }
+        else{
+            this.right.addNode(n)
+        }
+        
+    }
+}
+Node.prototype.visit=function(){
+    if(this.left!==null){
+        this.left.visit()
+    }
+    console.log(this.value)
+    if(this.right!==null){
+        this.right.visit()
+    }
+    
+}
+function Node(val){
+    this.value=val
+    this.left=null
+    this.right=null
+}
---
+++ BinarySearchTree/script.js
@@ -0,0 +1,16 @@
+var tree
+function setup(){
+    
+    tree=new Tree()
+    tree.addValue(5)
+    tree.addValue(4)
+    tree.addValue(43)
+    tree.addValue(2)
+    tree.traverse()
+    console.log(tree)
+}
+
+
+
+
+
